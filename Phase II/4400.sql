4ac0a07506e1c02bb7752027c2c01e89f87e5797
IvoryCandy <deovant@gmail.com>
2017-10-19 16:45:10 -0400

    edit sql file

--- Phase II/4400.sql
+++ Phase II/4400.sql
@@ -1,46 +1,40 @@
-CREATE TABLE User
+CREATE TABLE USER
 (
-	Username varchar(255) NOT NULL,
-	Password varchar(255) NOT NULL,
+	Username varchar(15) NOT NULL,
+	Password varchar(15) NOT NULL,
 	PRIMARY KEY (Username)
 );
-/*Username is unique*/
 
-CREATE TABLE Admin
+CREATE TABLE PASSENGER
 (
-	admin_Username varchar(255) NOT NULL,
-	PRIMARY KEY (Admin_Username)
-	CONSTRAINT 'FK Admin' FOREIGN KEY ('admin_Username') REFERENCES 'User' ('Username')
-	ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-CREATE TABLE Passenger
-(
-	passenger_Username varchar(255) NOT NULL,
-	Email varchar(255) NOT NULL,
+	passenger_Username varchar(15) NOT NULL,
+	Passenger_Email varchar(25) NOT NULL,
 	PRIMARY KEY (Passenger_Username)
-	CONSTRAINT CHECK Email
-	CONSTRAINT 'FK Passenger' FOREIGN KEY ('passenger_Username') REFERENCES 'User' ('Username')
+	UNIQUE (Passenger_Email)
+	CONSTRAINT CHECK Passenger_Email
+	FOREIGN KEY ('passenger_Username') REFERENCES USER('Username')
 	ON DELETE CASCADE ON UPDATE CASCADE
 );
+
 /*Username must be unique.
 Email addr must be unique
 password at least 8 char
 must have at least 1 BreezeCard
-Email addr kdjfbhsk@nkjds.snkd*/
+Email addr match '^[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$' */
 
-CREATE TABLE BreezeCard
+CREATE TABLE BREEZECARD
 (
-	card_Number int(16) NOT NULL,
-	card_Value varchar(255) NOT NULL,
-	PassengerEmail varchar(255) DEFAULT NULL,
-	PRIMARY KEY (Card_number)
-	CONSTRAINT 'FK BreezeCard' FOREIGN KEY ('PassengerEmail') REFERENCES 'Passenger' ('Email')
+	Card_Number int(16) NOT NULL,
+	Card_Value char(6) NOT NULL,
+	Passenger_Username varchar(25) DEFAULT NULL,
+	PRIMARY KEY (Card_Number),
+	UNIQUE (Passenger_Username),
+	FOREIGN KEY ('Passenger_Username') REFERENCES PASSENGER ('Passenger_Username')
 	ON DELETE CASCADE ON UPDATE CASCADE
 );
 /*card_Value <= $1000.00*/
 
-CREATE TABLE Station
+CREATE TABLE STATION
 (
 	StopID varchar(255) NOT NULL,
 	enter_Fare varchar(255) NOT NULL,
@@ -50,62 +44,38 @@ CREATE TABLE Station
 /*Fare to enter: $0.00 to $50.00 inclusive*/
 
 
-CREATE TABLE BusStation
+CREATE TABLE BUSSTATION
 (
 	bus_station_Name varchar(255) NOT NULL,
 	station_StopID varchar(255) DEFAULT NULL,
 	intersection varchar(255) NOT NULL,
 	PRIMARY KEY (bus_station_Name, station_StopID)
-	CONSTRAINT 'FK BusStation' FOREIGN KEY ('station_StopID') REFERENCES 'Station' ('StopID')
+	FOREIGN KEY ('station_StopID') REFERENCES STATION ('StopID')
 	ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
-CREATE TABLE TrainStation
-(
-	train_station_Name varchar(255) NOT NULL,
-	station_StopID varchar(255) DEFAULT NULL,
-	PRIMARY KEY (train_station_Name, station_StopID)
-	CONSTRAINT 'FK TrainStation' FOREIGN KEY ('station_StopID') REFERENCES 'Station' ('StopID')
-	ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-CREATE TABLE Trip
+CREATE TABLE TRIP
 (
 	start_Time varchar(255) NOT NULL,
 	current_Fare varchar(255) NOT NULL,
 	BCNumber varchar(255) DEFAULT NULL,
 	station_StopID varchar(255) DEFAULT NULL,
 	PRIMARY KEY (start_Time, BCNumber)
-	CONSTRAINT 'FK Trip1' FOREIGN KEY ('station_StopID') REFERENCES 'Station' ('StopID')
+	FOREIGN KEY ('station_StopID') REFERENCES STATION ('StopID')
 	ON DELETE CASCADE ON UPDATE CASCADE
-	CONSTRAINT 'FK Trip2' FOREIGN KEY ('BCNumber') REFERENCES 'BreezeCard' ('card_Number')
+	FOREIGN KEY ('BCNumber') REFERENCES BREEZECARD ('card_Number')
 	ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Conflict
+CREATE TABLE CONFLICT
 (
 	PassengerEmail varchar(255) DEFAULT NULL,
 	BCNumber varchar(255) DEFAULT NULL,
 	date_Time varchar(255) NOT NULL,
 	PRIMARY KEY (PassengerEmail, BCNumber)
-	CONSTRAINT 'FK Conflict1' FOREIGN KEY ('PassengerEmail') REFERENCES 'Passenger' ('Email')
+	FOREIGN KEY ('PassengerEmail') REFERENCES PASSENGER ('Email')
 	ON DELETE CASCADE ON UPDATE CASCADE
-	CONSTRAINT 'FK Conflict2' FOREIGN KEY ('BCNumber') REFERENCES 'BreezeCard' ('card_Number')
+	FOREIGN KEY ('BCNumber') REFERENCES BREEZECARD ('card_Number')
 	ON DELETE CASCADE ON UPDATE CASCADE
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
