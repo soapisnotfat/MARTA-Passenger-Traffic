cc368153f4c646f0227a53b50ae2a30f2fd2617d
IvoryCandy <deovant@gmail.com>
2017-10-23 19:29:30 -0400

    update sql

--- Phase II/4400.sql
+++ Phase II/4400.sql
@@ -1,7 +1,7 @@
 CREATE TABLE USER
 (
 	Username	VARCHAR(15), 	NOT NULL,
-	Password 	VARCHAR(15), 	NOT NULL,
+	Password 	VARCHAR(15),
 	PRIMARY KEY (Username),
 );
 
@@ -24,7 +24,7 @@ Email addr match '^[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$'
 CREATE TABLE BREEZECARD
 (
 	Card_Number 		INT 			NOT	NULL,
-	Card_Value 			DECIMAL(6, 2), 	# or NOT NULL
+	Card_Value 			DECIMAL(6, 2), 	NOT NULL,
 	Passenger_Username 	VARCHAR(25),    # nor DEFAULT NULL
 	PRIMARY KEY (Card_Number),
 	UNIQUE (Passenger_Username),
@@ -36,8 +36,9 @@ CREATE TABLE BREEZECARD
 CREATE TABLE STATION
 (
 	StopID 			INT 			NOT NULL,
-	Enter_Fare 		DECIMAL(4, 2), 	# or NOT NULL
-	Closed_Status 	BOOLEAN,
+	Enter_Fare 		DECIMAL(4, 2), 	NOT NULL,
+	Closed_Status 	BOOLEAN			NOT NULl,
+	
 	PRIMARY KEY (StopID)
 );
 /*Fare to enter: $0.00 to $50.00 inclusive*/
@@ -48,7 +49,7 @@ CREATE TABLE BUSSTATION
 	Bus_Dtation_Name 	VARCHAR(30) 	NOT NULL,
 	Station_StopID 		INT 			NOT NULL,
 	Intersection 		VARCHAR(30),
-	PRIMARY KEY (Bus_Dtation_Name, Station_StopID)
+	PRIMARY KEY (Bus_Dtation_Name, Station_StopID),
 	FOREIGN KEY ('Station_StopID') REFERENCES STATION ('StopID')
 	ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -57,11 +58,11 @@ CREATE TABLE BUSSTATION
 CREATE TABLE TRIP
 (
 	Start_Time 		CHAR(19) 		NOT NULL, 	# YYYY-MM-DD HH:MM:SS
-	Current_Fare 	DECIMAL(4, 2), 	# or NOT NULL
+	Current_Fare 	DECIMAL(4, 2), 	NOT NULL,
 	BCNumber 		INT 			NOT NULL,
-	Station_StopID 	INT, 			# or NOT NULL
+	Station_StopID 	INT, 			NOT NULL,
 	PRIMARY KEY (Start_Time, BCNumber)
-	FOREIGN KEY ('Station_StopID') REFERENCES STATION ('StopID')
+	FOREIGN KEY ('Station_StopID') REFERENCES STATION ('StopID'),
 	ON DELETE CASCADE ON UPDATE CASCADE
 	FOREIGN KEY ('BCNumber') REFERENCES BREEZECARD ('card_Number')
 	ON DELETE CASCADE ON UPDATE CASCADE
@@ -71,7 +72,7 @@ CREATE TABLE CONFLICT
 (
 	Passenger_Username 	VARCHAR(25)		NOT NULL,
 	BCNumber 			INT 			NOT NULL,
-	date_Time 			DATE,
+	date_Time 			DATE,			NOT NULL,
 	PRIMARY KEY (PassengerEmail, BCNumber)
 	FOREIGN KEY ('PassengerEmail') REFERENCES PASSENGER ('Email')
 	ON DELETE CASCADE ON UPDATE CASCADE
